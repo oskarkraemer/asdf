25adbc1cb3a0b7e93c14b7ffcea6e7890ac27642
oskarkraemer <42745862+oskarkraemer@users.noreply.github.com>
2021-01-29 22:51:26 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+#include <sstream>
+
+#include "bitmap_image.h"
+#include "FastNoiseLite.h"
+
+int main(int argc, char* argv[]) {
+
+	//Arguments: <amount> <width> <height> <R> <G> <B> <outputPath>
+
+	if (argc < 8) {
+		std::cout << "Arguments: <amount> <width> <height> <R> <G> <B> <outputPath>" << std::endl;
+	}
+	else {
+		const int width = atoi(argv[2]);
+		const int height = atoi(argv[3]);
+
+		bitmap_image image(width, height);
+		image.set_all_channels(0, 0, 0);
+
+		//Initialize Noise
+		FastNoiseLite noise;
+		noise.SetNoiseType(FastNoiseLite::NoiseType_Perlin);
+
+		int start_time = time(0);
+
+		for (int i = 0; i < atoi(argv[1]); i++) {
+				noise.SetSeed(start_time+i);
+
+				image_drawer draw(image);
+
+
+
+				for (int x = 0; x < width; x++) {
+					float noiseValue = noise.GetNoise((float)(x * .2), .0f);
+						float noiseValue2;
+						if (x == width - 1) {
+							noiseValue2 = noise.GetNoise((float)(x * .2), .0f);
+						}
+						else {
+							noiseValue2 = noise.GetNoise((float)((x + 1) * .2), .0f);
+						}
+
+					if (noiseValue < 0) { noiseValue *= -1; }
+
+					draw.pen_color(((atoi(argv[4]) / 255)*noiseValue)*255, ((atoi(argv[5]) / 255) * noiseValue) * 255, ((atoi(argv[6]) / 255)*noiseValue)*255);
+					draw.line_segment(x, height - noiseValue * height, height - x + 1, noiseValue2 * height);
+
+				}
+
+				std::stringstream ss;
+				ss << argv[7] << i << ".bmp";
+
+				image.save_image(ss.str());
+				image.clear();
+		}
+
+	}
+
+	return 0;
+}
